1737f6fed4711571ab98682aeb4acd65dfdcc369
edcan-dev <israel.da.queenie@gmail.com>
2023-04-24 17:40:29 -0600

    asu

---
+++ src/main/java/com/okysoft/azureadexam/AzureAdExamApplication.java
@@ -0,0 +1,13 @@
+package com.okysoft.azureadexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureAdExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureAdExamApplication.class, args);
+    }
+
+}
